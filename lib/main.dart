d7511324a55ac4be3cf010e70ee06870cf71f4e9
Abdelwaheb <bouchahwaabdelwaheb@fsg.u-gabes.tn>
2025-08-18 18:35:44 +0100

    + Splash screen loading

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,52 @@
 import 'package:flutter/material.dart';
 import 'package:restaurant_universitaire/screens/home_screen.dart';
+import 'package:restaurant_universitaire/screens/splash_screen.dart';
+import 'package:restaurant_universitaire/theme/app_theme.dart';
 
 void main() {
   runApp(const RestaurantApp());
 }
+//
+// class RestaurantApp extends StatelessWidget {
+//   const RestaurantApp({super.key});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Restaurant Universitaire Wahat',
+//       debugShowCheckedModeBanner: false,
+//       theme: AppTheme.lightTheme,
+//       home: const HomeScreen(),
+//     );
+//   }
+// }
 
-class RestaurantApp extends StatelessWidget {
+class RestaurantApp extends StatefulWidget {
   const RestaurantApp({super.key});
 
   @override
+  State<RestaurantApp> createState() => _RestaurantAppState();
+}
+
+class _RestaurantAppState extends State<RestaurantApp> {
+  bool _isLoading = true;
+  @override
+  void initState() {
+    Future.delayed(Duration(seconds: 2), () {
+      setState(() {
+        _isLoading = false;
+      });
+    });
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       title: 'Restaurant Universitaire Wahat',
       debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
+      theme: AppTheme.lightTheme,
+      home: _isLoading ? SplashScreen() : HomeScreen(),
     );
   }
 }
