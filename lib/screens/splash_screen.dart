a2b53c1c6bcd856503bc6dee58a2df19471eed2d
Abdelwaheb <bouchahwaabdelwaheb@fsg.u-gabes.tn>
2025-08-22 00:54:54 +0100

    check if the user have a session or login

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,7 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:restaurant_universitaire/main.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    _redirect();
+  }
+
+  Future<void> _redirect() async {
+    await Future.delayed(Duration.zero);
+    final session = supabase.auth.currentSession;
+    if (!mounted) return;
+
+    if (session != null) {
+      Navigator.of(context).pushReplacementNamed('/home');
+    } else {
+      Navigator.of(context).pushReplacementNamed('/login');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
