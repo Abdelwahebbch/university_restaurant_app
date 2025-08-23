81ae583580ccfe27c14bff6e4bbcab2a0db42599
Abdelwaheb <bouchahwaabdelwaheb@fsg.u-gabes.tn>
2025-08-23 17:08:58 +0100

    FireBase config

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,10 +1,20 @@
 plugins {
     id("com.android.application")
+    id("com.google.gms.google-services")
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
 
+dependencies {
+    // Import the Firebase BoM
+    implementation(platform("com.google.firebase:firebase-bom:34.1.0"))
+
+
+    // TODO: Add the dependencies for Firebase products you want to use
+    // When using the BoM, don't specify versions in Firebase dependencies
+    // https://firebase.google.com/docs/android/setup#available-libraries
+}
 android {
     namespace = "com.example.university_restaurant_app"
     compileSdk = flutter.compileSdkVersion
@@ -21,10 +31,10 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.university_restaurant_app"
+        applicationId = "com.abdelwaheb.wahat"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        minSdkVersion (23)
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
